27ac68f36d4e044e6ff8c48e9a480eb7498c5af5
anackiyVadim <160025058+anackiyVadim@users.noreply.github.com>
2024-04-01 20:50:07 +0300

    Add files via upload

---
+++ gallery/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from .models import Photo
+from .forms import PhotoForm
+
+def gallery(request):
+    photos = Photo.objects.all()
+    return render(request, "gallery/index.html", {"photos":photos})
+
+def upload(request):
+    if request.method == 'POST':
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('gallery')
+
+    form = PhotoForm()
+    return render(request, "gallery/upload.html", {"form": form})
